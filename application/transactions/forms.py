e0cfed7f6136851a4978d9c6024c0b1941442ff2
Otto Lasma <otto.lasma@aalto.fi>
2020-05-18 22:27:03 +0300

    editointi lisätty - toimii atm jotenkin

--- application/transactions/forms.py
+++ application/transactions/forms.py
@@ -11,6 +11,7 @@ class TransactionForm(FlaskForm):
     amount = IntegerField("Amount", [validators.NumberRange(min=None, max=None, message="Value must be an integer")])
     class Meta:
         csrf = False
+        
 class TransactionEditForm(FlaskForm):
     currency = StringField('Currency', [validators.Length(min=3, max=5, message="Please use the abbreviation of the currency in question"), valid_currency])
     amount = IntegerField("Amount", [validators.NumberRange(min=None, max=None, message="Value must be an integer")])
--- application/transactions/views.py
+++ application/transactions/views.py
@@ -16,6 +16,11 @@ def transactions_index():
 def transactions_form():
     return render_template("transactions/new.html", form=TransactionForm())
 
+@app.route("/transactions/edit/<transaction_id>")
+@login_required
+def transactionsedit_form(transaction_id):
+    t = Transaction.query.get(transaction_id)
+    return render_template("transactions/edit.html", form=TransactionEditForm(), transaction = t)
 
 
 @app.route("/transactions/", methods=["GET", "POST"])
@@ -23,28 +28,30 @@ def transactions_form():
 def transactions_create():
     form = TransactionForm(request.form)
     if not form.validate():
-        return render_template("transactions/new.html", form=form)
+        return render_template("transactions/edit.html", form=form)
     t = Transaction(form.currency.data, form.amount.data)
     t.account_id = current_user.id
     db.session().add(t)
     db.session().commit()
     return redirect(url_for("index"))
 
-@app.route("/transactions/edit/<transaction_id>", methods=["GET", "POST"])
-@login_required
-def edit_transaction(transaction_id):
-    if request.method == "GET":
-        transaction = Transaction.query.get(transaction_id)
-        form = TransactionEditForm(currency = transaction.currency, amount = transaction.amount)
-        return render_template("transactions/edit.html", form = form)
-    form = TransactionEditForm(request.form)
-    transaction = Transaction.query.get(transaction_id)
-    if not form.validate():
-        return render_template("transactions/edit.html", form=form)
+def save_edit(transaction, form, new = False):
     transaction.currency = form.currency.data
     transaction.amount = form.amount.data
     db.session().commit()
-    return redirect(url_for("transactions/list.html"))
+    return render_template("transactions/list.html", transactions=Transaction.query.filter_by(account_id = current_user.id))
+
+@app.route("/transactions/edit/<transaction_id>", methods=["GET", "POST"])
+@login_required
+def edit_transaction(transaction_id):
+    
+    transaction = Transaction.query.get(transaction_id)
+    form = TransactionEditForm(request.form, obj= transaction)
+    if request.method == "POST":
+        save_edit(transaction, form)
+        return render_template("transactions/list.html", transactions=Transaction.query.filter_by(account_id = current_user.id))
+    
+    return render_template("transactions/list.html", transactions=Transaction.query.filter_by(account_id = current_user.id))
 
 @app.route("/transactions/delete/<transaction_id>", methods=["POST"])
 @login_required
